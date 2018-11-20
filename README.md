99234da9632b21b089dfe91a161426771be8d4d5
Peter Jørgensen <pj@webstarters.dk>
2018-11-20 09:29:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Moduler
+
+Et WordPress-plugin til at opbygge moduler i Visual Composer og indsæt via shortcode, widgets, m.m.
+
+## Credit
+
+- [Peter Jørgensen](https://github.com/peterchrjoergensen)
+- [All Contributors](../../contributors)
+
+## License
+
+© Webstarters A/S.
---
+++ webstarters-moduler/webstarters-moduler.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package           webstarters-moduler
+ * @version           1.1.0
+ * @link              https://webstarters.dk
+ *
+ * Plugin Name:       Webstarters Moduler
+ * Plugin URI:        https://webstarters.dk
+ * Description:       Et plugin til at opbygge moduler og indsætte dem via Visual Composer på sider
+ * Version:           1.1.0
+ * Author:            Webstarters
+ * Author URI:        https://webstarters.dk
+ */
+
+// If this file is called directly, abort.
+if (! defined('ABSPATH')) {
+    die;
+}
+
+if (! defined('MODUL_DIR')) {
+    define('MODUL_DIR', dirname(__FILE__));
+}
+
+// Load in our classes.
+require_once(MODUL_DIR . '/lib/modul.php');
+require_once(MODUL_DIR . '/lib/visualcomposer.php');
+require_once(MODUL_DIR . '/lib/toolbar.php');
+require_once(MODUL_DIR . '/lib/widget.php');
+require_once(MODUL_DIR . '/lib/shortcode.php');
+
+// Instantiate classes
+new Modul();
+new ModulWPBakeryShortCode();
+
+// Register the systems.
+add_action('widgets_init', function () {
+  register_widget('modulWidget');
+});
+
+?>
