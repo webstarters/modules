fea50b5d52297b97c49a67fb53c585fa27bae4e5
Peter Jørgensen <pj@webstarters.dk>
2018-11-21 10:57:20 +0100

    Bugfix WPBakeryShortcode class doesnt exist

--- webstarters-moduler/lib/visualcomposer.php
+++ webstarters-moduler/lib/visualcomposer.php
@@ -1,63 +1,64 @@
 <?php
 
-class ModulWPBakeryShortCode extends WPBakeryShortCode {
-
-    function __construct() {
-        add_action('init', array($this, 'modul_shortcode_mapping'));
-        add_shortcode('modul', array($this, 'modul_shortcode_html'));
-    }
-
-    public function modul_shortcode_mapping() {
-        if (! defined('WPB_VC_VERSION')) {
-            return;
+if (class_exists('WPBakeryShortCode')) {
+    class ModulWPBakeryShortCode extends WPBakeryShortCode {
+        function __construct() {
+            add_action('init', array($this, 'modul_shortcode_mapping'));
+            add_shortcode('modul', array($this, 'modul_shortcode_html'));
         }
 
-        $posts = get_posts([
-            'posts_per_page'    => -1,
-            'post_type'         => 'modul',
-            'orderby'           => 'date',
-            'order'             => 'DESC',
-        ]);
+        public function modul_shortcode_mapping() {
+            if (! defined('WPB_VC_VERSION')) {
+                return;
+            }
 
-        $moduler = [];
-        foreach($posts as $post) {
-            $moduler['id-'. (string) $post->ID] = $post->post_title;
+            $posts = get_posts([
+                'posts_per_page'    => -1,
+                'post_type'         => 'modul',
+                'orderby'           => 'date',
+                'order'             => 'DESC',
+            ]);
+
+            $moduler = [];
+            foreach($posts as $post) {
+                $moduler['id-'. (string) $post->ID] = $post->post_title;
+            }
+
+            vc_map([
+                'name'              => __('Modul', 'webstarters'),
+                'base'              => 'modul',
+                'description'       => __('Indsæt et modul', 'webstarters'),
+                'category'          => __('Webstarters', 'webstarters'),
+                "icon"              => MODUL_DIR . "/assets/img/modul_512x512.png",
+                'params'            => [
+                    [
+                        'type'          => 'dropdown',
+                        'heading'       => __( 'Modul', 'webstarters'),
+                        'description'   => __('Vælg et modul', 'webstarters'),
+                        'param_name'    => 'modul',
+                        'holder'        => 'p',
+                        'value'         => array_flip($moduler),
+                        'std'           => 'Intet valgt',
+                    ],
+                ],
+            ]);
         }
 
-        vc_map([
-            'name'              => __('Modul', 'webstarters'),
-            'base'              => 'modul',
-            'description'       => __('Indsæt et modul', 'webstarters'),
-            'category'          => __('Webstarters', 'webstarters'),
-            "icon"              => MODUL_DIR . "/assets/img/modul_512x512.png",
-            'params'            => [
-                [
-                    'type'          => 'dropdown',
-                    'heading'       => __( 'Modul', 'webstarters'),
-                    'description'   => __('Vælg et modul', 'webstarters'),
-                    'param_name'    => 'modul',
-                    'holder'        => 'p',
-                    'value'         => array_flip($moduler),
-                    'std'           => 'Intet valgt',
-                ],
-            ],
-        ]);
-    }
+        public function modul_shortcode_html($atts) {
 
-    public function modul_shortcode_html($atts) {
+            extract(
+                shortcode_atts(
+                    [
+                        'modul' => 'X',
+                    ],
+                    $atts
+                )
+            );
 
-        extract(
-            shortcode_atts(
-                [
-                    'modul' => 'X',
-                ],
-                $atts
-            )
-        );
+            $modul = str_replace('id-', '', $modul);
 
-        $modul = str_replace('id-', '', $modul);
-
-        return do_shortcode('[vis_modul modul="'. $modul .'"]');
+            return do_shortcode('[vis_modul modul="'. $modul .'"]');
+        }
     }
 }
 
